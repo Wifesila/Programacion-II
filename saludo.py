1225abbb6bcfb7fe13040d392a11ee74f629e42c
Wifesila <u20212201935@usco.edu.co>
2023-08-13 18:30:33 -0500

    Lo nuevo

--- saludo.py
+++ saludo.py
@@ -1,11 +1,41 @@
-print("Hola mundo")
+import os
+os.system("cls")
+print()
 
-a = 1 + 1
-b = 2 - 3
-c = 5 / 5
-d = 2 * 2
+def suma(a, b):
+    return a + b
 
-print(a)
-print(b)
-print(c)
-print(d)
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "No es posible dividir entre cero"
+
+operacion = input("Ingrese la operación (+, -, *, /): ")
+
+if operacion in ('+', '-', '*', '/'):
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    if operacion == '+':
+        resultado = suma(num1, num2)
+    elif operacion == '-':
+        resultado = resta(num1, num2)
+    elif operacion == '*':
+        resultado = multiplicacion(num1, num2)
+    elif operacion == '/':
+        resultado = division(num1, num2)
+
+    print(f"El resultado de {num1} {operacion} {num2} es: {resultado}")
+else:
+    print("Operación no válida. Por favor, ingrese una operación válida.")
+
+print()
+os.system('pause')
+os.system('cls')
