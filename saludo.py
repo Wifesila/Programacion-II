98b0c43934e654a6a4e88b60b71b44078777c3df
Wifesila <u20212201935@usco.edu.co>
2023-08-13 21:17:48 -0500

    13/08/2023

--- saludo.py
+++ saludo.py
@@ -15,26 +15,40 @@ def division(a, b):
     if b != 0:
         return a / b
     else:
-        return "No es posible dividir entre cero"
+        raise ValueError("No se puede dividir entre cero.")
 
-operacion = input("Ingrese la operación (+, -, *, /): ")
+while True:
+    try:
+        operacion = input("Ingrese la operación ( +, -, *, / ) o la letra 's' para salir: ")
 
-if operacion in ('+', '-', '*', '/'):
-    num1 = float(input("Ingrese el primer número: "))
-    num2 = float(input("Ingrese el segundo número: "))
+        if operacion == 's':
+            break
 
-    if operacion == '+':
-        resultado = suma(num1, num2)
-    elif operacion == '-':
-        resultado = resta(num1, num2)
-    elif operacion == '*':
-        resultado = multiplicacion(num1, num2)
-    elif operacion == '/':
-        resultado = division(num1, num2)
+        if operacion in ('+', '-', '*', '/'):
+            num1 = float(input("Ingrese el primer número: "))
+            num2 = float(input("Ingrese el segundo número: "))
 
-    print(f"El resultado de {num1} {operacion} {num2} es: {resultado}")
-else:
-    print("Operación no válida. Por favor, ingrese una operación válida.")
+            if operacion == '+':
+                resultado = suma(num1, num2)
+            elif operacion == '-':
+                resultado = resta(num1, num2)
+            elif operacion == '*':
+                resultado = multiplicacion(num1, num2)
+            elif operacion == '/':
+                resultado = division(num1, num2)
+
+            print()
+            print(f"El resultado de {num1} {operacion} {num2} es igual a: {resultado}")
+            print()
+        else:
+            print("||||               !!! Operación inválida ¡¡¡               ||||")
+            print("Por favor, digite una operación indicada.")
+            print()
+
+    except ValueError as e:
+        print("                    ##### Error #####                    ")
+        print("Digite solo lo que se pide e intente de nuevo por favor.")
+        print()
 
 print()
 os.system('pause')
