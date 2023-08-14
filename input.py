2e04e861e723f2b95341c327b48d360493fdc8d7
Wifesila <u20212201935@usco.edu.co>
2023-08-13 19:53:18 -0500

    Nuevo!!

--- input.py
+++ input.py
@@ -20,44 +20,46 @@ print("###########################################")
 
 #--------------------
 
-def suma(a, b):
-    return a + b
+# import os
+# os.system("cls")
 
-def resta(a, b):
-    return a - b
+# def suma(a, b):
+#     return a + b
 
-def multiplicacion(a, b):
-    return a * b
+# def resta(a, b):
+#     return a - b
 
-def division(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "##### Error #####. No se puede dividir entre cero."
+# def multiplicacion(a, b):
+#     return a * b
 
-while True:
-    print("Calculadora sencilla")
-    print("A continuacion digite los numeros que quiera")
-    print("Usando las peraciones disponibles: suma -->(+), resta -->(-), multiplicar -->(*), dividir -->(/)")
+# def division(a, b):
+#     if b != 0:
+#         return a / b
+#     else:
+#         return "##### Error #####. No se puede dividir entre cero."
+
+# while True:
+#     print("Calculadora sencilla")
+#     print("A continuacion digite los numeros que quiera")
+#     print("Usando las peraciones disponibles: suma -->(+), resta -->(-), multiplicar -->(*), dividir -->(/)")
     
-    num1 = float(input("Ingrese el primer número: "))
-    operacion = input("Ingrese el simbolo de la operación que desea realizar |+|,|-|,|*| o |/|): ")
-    num2 = float(input("Ingrese el segundo número: "))
+#     num1 = float(input("Ingrese el primer número: "))
+#     operacion = input("Ingrese el simbolo de la operación que desea realizar |+|,|-|,|*| o |/|): ")
+#     num2 = float(input("Ingrese el segundo número: "))
     
-    if operacion == "+":
-        resultado = suma(num1, num2)
-    elif operacion == "-":
-        resultado = resta(num1, num2)
-    elif operacion == "*":
-        resultado = multiplicacion(num1, num2)
-    elif operacion == "/":
-        resultado = division(num1, num2)
-    else:
-        print("Elija una opción correcta por favor")
-        continue
+#     if operacion == "+":
+#         resultado = suma(num1, num2)
+#     elif operacion == "-":
+#         resultado = resta(num1, num2)
+#     elif operacion == "*":
+#         resultado = multiplicacion(num1, num2)
+#     elif operacion == "/":
+#         resultado = division(num1, num2)
+#     else:
+#         print("Elija una opción correcta por favor")
+#         continue
     
-    print("Resultado:", resultado)
+#     print("Resultado:", resultado)
 
-os.system('pause')
-
-
+# os.system('pause')
+# os.system('cls')
