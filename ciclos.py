4af5965cd8dc0620234f8b72958fd12bfc9fe1bb
Wifesila <u20212201935@usco.edu.co>
2023-08-15 18:45:35 -0500

    15/08/2023

---
+++ ciclos.py
@@ -0,0 +1,28 @@
+#while
+
+# varControl = 1
+
+# while varControl <= 10:
+#     print(varControl)
+#     varControl +=1
+#----------------------------------
+# listado = ["juan" , "pedro" , "maria" , "sofia"]
+# b = 0
+# while b < len(listado):
+#     print(listado[b])
+#     b +=1
+#----------------------------------
+# ciclo for
+
+frutas = ["limon", "fresa", "pera", "manzana"]
+
+for i in frutas:
+    print(i)
+
+print("-----------------")
+for a in range(10):
+    print(a)
+print("-----------------")
+for a in range(2, 20, 2):
+    print(a)
+
---
+++ tallerciclos.py
@@ -0,0 +1,20 @@
+#1. Escriba un programa que almacene la cadena de caracteres contraseña en una 
+# variable, para luego preguntarle al usuario por la contraseña. 
+#Luego, imprima en la consola si la contraseña que el usuario ingreso coincide con 
+# la guardada en variable.
+
+#2. Realice un programa que le pida al usuario dos números por teclado y muestre por 
+# consola su división. Si el divisor es cero el programa debe mostrar un error y solicitar
+#nuevamente el numero.
+
+#3. Escriba un programa que le pida al usuario por teclado un numero entero y 
+# muestre en consola si es par o impar.
+
+#4. Escriba que mediante un vector  de 5 item, lea cada item y evalué el ingreso a menores de 
+# edad, si la persona tiene menos de 19 años el programa le debe mostrar 
+#en pantalla que ¡No puede ingresar!, de caso contrario que le diga ¡Bienvenido!
+
+ingrContraseña = input("Escriba una contraseña por favor. ")
+
+print(ingrContraseña)
+
