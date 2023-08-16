972dd7c2de007fd581d3ae89a140e8c35307056f
Wifesila <u20212201935@usco.edu.co>
2023-08-16 16:24:42 -0500

    TP

--- tallerciclos.py
+++ tallerciclos.py
@@ -10,85 +10,59 @@
 #3. Escriba un programa que le pida al usuario por teclado un numero entero y 
 # muestre en consola si es par o impar.
 
-#4. Escriba que mediante un vector  de 5 item, lea cada item y evalué el ingreso a menores de 
+#4. Escriba que mediante un vector de 5 item, lea cada item y evalué el ingreso a menores de 
 # edad, si la persona tiene menos de 19 años el programa le debe mostrar 
 #en pantalla que ¡No puede ingresar!, de caso contrario que le diga ¡Bienvenido!
+
 import os
 os.system("cls")
 print("--------------------------------------")
 
-print("PRIMER EJERCICIO")
-sistContraseña = '2023'
-ingrContraseña2 = input("Escriba la contraseña del sistema: ")
+# print("PRIMER EJERCICIO")
+# sistContraseña = '2023'
+# ingrContraseña2 = input("Escriba la contraseña del sistema: ")
 
-if ingrContraseña2 == sistContraseña:
-    print("Acceso concedido. ")
-else:
-    print("X acceso denegado X")
+# if ingrContraseña2 == sistContraseña:
+#     print("Acceso concedido. ")
+# else:
+#     print("X acceso denegado X")
 
-
-print("SEGUNDO EJERCICIO")
-def main():
-    while True:
-        try:
-            dividendo = float(input("Ingrese el dividendo: "))
-            divisor = float(input("Ingrese el divisor: "))
+# print("SEGUNDO EJERCICIO")
+# while True:
+#         try:
+#             dividendo = float(input("Ingrese el dividendo: ")) 
+#             divisor = float(input("Ingrese el divisor: "))
             
-            if divisor == 0:
-                print("Error: No se puede dividir por cero. Intente nuevamente.")
-            else:
-                division = dividendo / divisor
-                print(f"La división de {dividendo} entre {divisor} es: {division}")
-                break
-        except ValueError:
-            print("Error: Ingrese números válidos.")
+#             if divisor == 0:
+#                 print("Error: No se puede dividir por cero. Intente nuevamente.")
+#             else:
+#                 division = dividendo / divisor
+#                 print("La división de", dividendo, "entre", divisor,"es:", division)
+#                 break
+#         except ValueError:
+#             print("Error: Ingrese números válidos.")
 
-if __name__ == "__main__":
-    main()
-
-
-
-print("TERCER EJERCICIO")
-def main():
-    try:
-        numero = int(input("Ingrese un número entero: "))
-        
-        if numero % 2 == 0:
-            print(f"El número {numero} es par.")
-        else:
-            print(f"El número {numero} es impar.")
-    except ValueError:
-        print("Error: Ingrese un número entero válido.")
-
-if __name__ == "__main__":
-    main()
+# print("TERCER EJERCICIO")
+# while True:
+#     numero = int(input("Ingrese un número entero: "))
+#     if numero % 2 == 0:
+#         print("El número" , numero, "es par.")  
+#         break
+#     elif numero % 2 != 0:
+#         print("El número" ,numero, "es impar.")
+#         break
+#     else:
+#         print("Error: Ingrese un número entero válido.")
+#         break
 
 print("CUARTO EJERCICIO")
 
-def main():
-    vector_edades = []  # Creamos un vector para almacenar las edades
-
-    for i in range(5):
-        try:
-            edad = int(input(f"Ingrese la edad de la persona {i + 1}: "))
-            vector_edades.append(edad)
-        except ValueError:
-            print("Error: Ingrese una edad válida.")
-
-    for i, edad in enumerate(vector_edades):
-        if edad < 19:
-            print(f"Persona {i + 1}: No puede ingresar.")
-        else:
-            print(f"Persona {i + 1}: ¡Bienvenido!")
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-
-
+Personas = [18, 20, 30, 15, 12]
+for e in Personas:
+    if e < 19:
+        print("No puede ingresar.")
+    else:
+        print("Bienvenido")
 
 print("--------------------------------------")
 os.system('pause')
