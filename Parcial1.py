6d2e5b88eb2d7fed3ec000dc6c1337df8535b7c9
Wifesila <u20212201935@usco.edu.co>
2023-08-23 18:23:49 -0500

    Lo del parcial 1

---
+++ Parcial1.py
@@ -0,0 +1,37 @@
+import os
+os.system("cls")
+print()
+#-------------------------------------------
+
+# 1. Escriba un programa que almacene (Input) en una Lista las materias que has cursado con sus respectivas notas
+# . Luego muestre la lista por consola mediante un ciclo.
+
+numerodematerias = int(input("Escriba el numero de cursos que has visto: "))
+notasdelasmaterias = []
+
+for x in range(numerodematerias):
+    
+
+
+# 2. Escriba un programa que almacene personas (input), luego que le muestre 
+# por pantalla el mensaje de ‘Su nombre es ‘nombre’
+
+
+
+
+# 3. Escribir un programa que guarde en una variable un diccionario con los siguientes valores 
+# {'Euro':'€', 'Dollar':'$', 'Yen':'¥'} Luego pregunte al usuario por una divisa y el valor en pesos a convertir. Luego muestre en consola el 
+# símbolo con el valor que corresponde a la divisa o un mensaje de advertencia si esa divisa no se encuentra en el diccionario.
+
+
+
+
+
+
+# 4. En una tupla coloque o ingrese (input) los siguientes valores: números enteros, 
+# decimales, String, colecciones. Luego muestre en consola que tipo de datos o variable son los valores digitados.
+
+#-------------------------------------------
+print()
+os.system('pause')
+os.system('cls')
