4f24c6b9401d614d7de09ff2b83c8e14e7640c96
Wifesila <u20212201935@usco.edu.co>
2023-08-23 19:08:52 -0500

    Hasta el 3

--- Parcial1.py
+++ Parcial1.py
@@ -6,30 +6,45 @@ print()
 # 1. Escriba un programa que almacene (Input) en una Lista las materias que has cursado con sus respectivas notas
 # . Luego muestre la lista por consola mediante un ciclo.
 
-numerodematerias = int(input("Escriba el numero de cursos que has visto: "))
-notasdelasmaterias = []
+# notasdelasmaterias = []
 
-for x in range(numerodematerias):
-    
+# numerodematerias = int(input("Escriba el numero de cursos que has visto: "))
 
+# for i in range(numerodematerias):
+#     curso = input(f"Escriba el nombre del curso {i + 1} por favor: ")
+#     nota = float(input(f"Escriba la nota del curso {i + 1} por favor: "))
+#     notasdelasmaterias.append((curso, nota))
+
+# print("Notas definitivas: ")
+# for curso, nota in notasdelasmaterias:
+#     print(f"Del curso: {curso}, su nota es: {nota}")
 
 # 2. Escriba un programa que almacene personas (input), luego que le muestre 
 # por pantalla el mensaje de ‘Su nombre es ‘nombre’
 
-
+# comosellama = input("Escriba su nombre por favor: ")
+# print(f"Usted se llama {comosellama}")
 
 
 # 3. Escribir un programa que guarde en una variable un diccionario con los siguientes valores 
 # {'Euro':'€', 'Dollar':'$', 'Yen':'¥'} Luego pregunte al usuario por una divisa y el valor en pesos a convertir. Luego muestre en consola el 
 # símbolo con el valor que corresponde a la divisa o un mensaje de advertencia si esa divisa no se encuentra en el diccionario.
 
+Divisas = {"Euro": "€", "Dollar": "$", "Yen": "¥"}
 
+Divisa = input("Escriba una divisa: ")
+Valorenpesos = float(input("Escriba el valor en pesos a convertir por favor: "))
 
-
-
+if Divisa in Divisas:
+    Simbolo = Divisas[Divisa]
+    print(f"{Simbolo}{Valorenpesos}")
+else:
+    print("Esa divisa no se encuentra en el diccionario.") 
 
 # 4. En una tupla coloque o ingrese (input) los siguientes valores: números enteros, 
 # decimales, String, colecciones. Luego muestre en consola que tipo de datos o variable son los valores digitados.
+
+
 
 #-------------------------------------------
 print()
