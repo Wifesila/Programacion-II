7c8eb6fff17c5377498a56bd683977f60ea408d8
Wifesila <u20212201935@usco.edu.co>
2023-09-06 14:52:49 -0500

    VamosparaDelante

--- Tarea.py
+++ Tarea.py
@@ -1,32 +1,45 @@
 import os
 os.system("cls")
 print("-------------------------------------------")
+print()
 ################################################
-# 1. Escriba una función que muestre por consola un saludo personalizado. Por ejemplo ‘¡Hola mundo!’
+# 1. Escriba una función que muestre por consola un 
+# saludo personalizado. Por ejemplo ‘¡Hola mundo!’
 
+# def saludo(nombre): # Definicion de una funcion que toma el parametro nombre
+#     mensaje = f"¡Hola {nombre}!" # Dentro de la funcion se coloca una variable con una cadena formateada (f-string), donde {} es una marcador de posicion
+#     print(mensaje) # Imprime el valor de la variable
 
-
-
-################################################
-# 2. Escriba una función que reciba un nombre por parámetro y que luego muestre en pantalla ¡Hola <nombre>!
-
-
-
-
-################################################
-# 3. Cree una función que le pida al usuario su nombre y su edad, luego muestre en pantalla los resultados.
-
-
-
-
+# saludopersonalizado = "compay como vas" # Variable que se le coloca el valor "compay..."
+# saludo(saludopersonalizado) # llamado a la funcion que toma como argumento "saludopersonalizado"
 
 ################################################
-# 4. Definir una función que reciba n cantidad de números por parámetros y que luego los sume, reste, mutiplique y divida.
+# 2. Escriba una función que reciba un nombre por parámetro
+# y que luego muestre en pantalla ¡Hola <nombre>!
+
+# def saluda(nombre): # Definicion de una funcion que toma el parametro nombre
+#     Loquevaadecir = f"¡Hola {nombre}!" # Dentro de la funcion se coloca una variable con una cadena formateada (f-string), donde {} es un marcador de posicion
+#     print(Loquevaadecir) # Imprime el valor de la variable
+
+# Elnombre = "Rodrigo Pepito" # Variable que se le coloca el valor "Rodri..."
+# saluda(Elnombre) # llamado a la funcion que toma como argumento "Elnombre" 
+
+################################################
+# 3. Cree una función que le pida al usuario su nombre y 
+# su edad, luego muestre en pantalla los resultados.
 
 
 
 
 
+################################################
+# 4. Definir una función que reciba n cantidad de números 
+# por parámetros y que luego los sume, reste, mutiplique y divida.
+
+
+
+
+print()
 print("-------------------------------------------")
 os.system('pause')
 os.system('cls')
