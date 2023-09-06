a4ce12cf86680e7f338cdaaacb35f145107c8f84
Wifesila <u20212201935@usco.edu.co>
2023-09-05 21:57:08 -0500

    ElNuevoArchivo

---
+++ Tarea.py
@@ -0,0 +1,32 @@
+import os
+os.system("cls")
+print("-------------------------------------------")
+################################################
+# 1. Escriba una función que muestre por consola un saludo personalizado. Por ejemplo ‘¡Hola mundo!’
+
+
+
+
+################################################
+# 2. Escriba una función que reciba un nombre por parámetro y que luego muestre en pantalla ¡Hola <nombre>!
+
+
+
+
+################################################
+# 3. Cree una función que le pida al usuario su nombre y su edad, luego muestre en pantalla los resultados.
+
+
+
+
+
+################################################
+# 4. Definir una función que reciba n cantidad de números por parámetros y que luego los sume, reste, mutiplique y divida.
+
+
+
+
+
+print("-------------------------------------------")
+os.system('pause')
+os.system('cls')
